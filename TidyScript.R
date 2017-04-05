b8fffd9591542036a4f2f1f7021424f687cb7897
Jay Lee <jaylee@reed.edu>
2017-04-04 21:22:25 -0700

    Spread version of ballot image data

--- TidyScript.R
+++ TidyScript.R
@@ -11,7 +11,8 @@ BallotImage <- read_tsv("data/20161206_ballotimage.txt", col_names = F) %>%
                  "over_vote",
                  "under_vote"),
            sep = c(7,16,23,26,33,36,43,44)) %>%
-  mutate(vote_rank = factor(vote_rank,
+  mutate(tally_type_id = as.integer(tally_type_id),
+         vote_rank = factor(vote_rank,
                             ordered = T,
                             levels = c("001","002","003")),
          vote_rank = fct_recode(vote_rank,
@@ -19,8 +20,7 @@ BallotImage <- read_tsv("data/20161206_ballotimage.txt", col_names = F) %>%
                                 "2" = "002",
                                 "3" = "003"),
          over_vote = as.integer(over_vote),
-         under_vote = as.integer(under_vote)) %>%
-  group_by(pref_voter_id, contest_id)
+         under_vote = as.integer(under_vote))
 
 MasterLookup <- read_tsv("data/20161206_masterlookup.txt", col_names = F) %>%
   separate(X1, c("record_type",
@@ -35,19 +35,33 @@ MasterLookup <- read_tsv("data/20161206_masterlookup.txt", col_names = F) %>%
          description = trimws(description),
          is_writein = as.integer(is_writein),
          is_provisional = as.integer(is_provisional))
+
 Candidates <- MasterLookup %>%
   filter(record_type == "Candidate") %>%
-  select(id, description)
+  select(id, description) %>%
+  rename(candidate = description)
 Contests <- MasterLookup %>%
   filter(record_type == "Contest") %>%
-  select(id, description)
+  select(id, description) %>%
+  rename(contest = description)
 Precincts <- MasterLookup %>%
   filter(record_type == "Precinct") %>%
-  select(id, description)
+  select(id, description) %>%
+  rename(precinct = description)
 Tallies <- MasterLookup %>%
   filter(record_type == "Tally Type") %>%
-  select(id, description)
+  select(id, description) %>%
+  mutate(id = as.integer(id)) %>%
+  rename(tally = description)
 
+BallotImage <- left_join(BallotImage, Candidates, by = c("candidate_id" = "id"))
+BallotImage <- left_join(BallotImage, Contests, by = c("contest_id" = "id"))
+BallotImage <- left_join(BallotImage, Precincts, by = c("precinct_id" = "id"))
+BallotImage <- left_join(BallotImage, Tallies, by = c("tally_type_id" = "id"))
+
+FinalImage <- BallotImage %>%
+  select(pref_voter_id, contest, vote_rank, candidate, precinct) %>%
+  spread(key = vote_rank, value = candidate)
 # Next step is to link/replace precinct_id with the name of the precinct,
 # candidate_id with the name of the candidate, etc.
 # Maybe some kind of join? 
