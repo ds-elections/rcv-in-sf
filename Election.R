05df468799efa3007e200d9dad517e3b970d28d4
Jay Lee <jaylee@reed.edu>
2017-04-05 20:36:15 -0700

    Round 1 of voting! Round 2 is off

--- Election.R
+++ Election.R
@@ -1,8 +1,24 @@
 District1 <- FinalImage %>%
-  filter(contest == "Board of Supervisors, District 1",
-         !is.na(`1`))
-Results <- District1 %>%
-  group_by(`1`) %>%
+  filter(contest == "Board of Supervisors, District 1") %>%
+  mutate(vote = `1`,
+         `2` = ifelse(is.na(`1`) | # Remove undervotes
+                        `2` == `1`, # Remove overvotes
+                      NA, `2`), 
+         `3` = ifelse(is.na(`1`) |
+                        `3` == `1` |
+                        `3` == `2`,
+                      NA, `3`))
+
+Round1 <- District1 %>%
+  group_by(vote) %>%
   summarise(Round1Total = n(),
             Round1Prop = Round1Total/nrow(District1)) %>%
   arrange(desc(Round1Total))
+
+Round2 <- District1 %>%
+  mutate(vote = ifelse(vote == as.character(Round1[nrow(Round1),1]),`2`,vote)) %>%
+  group_by(vote) %>%
+  summarise(Round2Total = n(),
+            Round2Prop = Round2Total/nrow(District1)) %>%
+  arrange(desc(Round2Total)) %>%
+  right_join(Round1, by = "vote")
