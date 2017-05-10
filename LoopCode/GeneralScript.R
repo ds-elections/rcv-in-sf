18f53b4b6e2c970b2cb0cc3eb9bbf4ed0173f7bb
Jay Lee <jaylee@reed.edu>
2017-05-09 18:45:22 -0700

    Comments added

--- LoopCode/GeneralScript.R
+++ LoopCode/GeneralScript.R
@@ -1,7 +1,8 @@
-# Works in the case of "California style" ballot images
 library(tidyverse)
 library(forcats)
-BallotImage <- read_tsv("http://www.acgov.org/rov/rcv/results/230/BerkeleyMayor/ballot_image.txt", col_names = F) %>%
+BallotImage <- read_tsv("http://www.acgov.org/rov/rcv/results/230/BerkeleyMayor/ballot_image.txt",
+                        # Insert link/file to ballot image above ^^
+                        col_names = F) %>%
   separate(X1, c("contest_id",
                  "pref_voter_id",
                  "serial_number",
@@ -12,6 +13,8 @@ BallotImage <- read_tsv("http://www.acgov.org/rov/rcv/results/230/BerkeleyMayor/
                  "over_vote",
                  "under_vote"),
            sep = c(7,16,23,26,33,36,43,44)) %>%
+# Break into distinct columns
+  
   mutate(tally_type_id = as.integer(tally_type_id),
          vote_rank = factor(vote_rank,
                             ordered = T,
@@ -23,7 +26,9 @@ BallotImage <- read_tsv("http://www.acgov.org/rov/rcv/results/230/BerkeleyMayor/
          over_vote = as.integer(over_vote),
          under_vote = as.integer(under_vote))
 
-MasterLookup <- read_tsv("http://www.acgov.org/rov/rcv/results/230/BerkeleyMayor/master_lookup.txt", col_names = F) %>%
+MasterLookup <- read_tsv("http://www.acgov.org/rov/rcv/results/230/BerkeleyMayor/master_lookup.txt",
+                         # Insert link/file to master lookup file above ^^
+                         col_names = F) %>%
   separate(X1, c("record_type",
                  "id",
                  "description",
@@ -32,11 +37,14 @@ MasterLookup <- read_tsv("http://www.acgov.org/rov/rcv/results/230/BerkeleyMayor
                  "is_writein",
                  "is_provisional"),
            sep = c(10,17,67,74,81,82)) %>%
+# Break into distinct columns
+  
   mutate(record_type = trimws(record_type),
          description = trimws(description),
          is_writein = as.integer(is_writein),
          is_provisional = as.integer(is_provisional))
 
+# Prepare "key" for joining with ballot image
 Candidates <- MasterLookup %>%
   filter(record_type == "Candidate") %>%
   select(id, description) %>%
@@ -55,6 +63,7 @@ Tallies <- MasterLookup %>%
   mutate(id = as.integer(id)) %>%
   rename(tally = description)
 
+# Join key to ballot image for each voter
 BallotImage <- left_join(BallotImage, Candidates, by = c("candidate_id" = "id"))
 BallotImage <- left_join(BallotImage, Contests, by = c("contest_id" = "id"))
 BallotImage <- left_join(BallotImage, Precincts, by = c("precinct_id" = "id"))
@@ -64,16 +73,19 @@ FinalImage <- BallotImage %>%
   select(pref_voter_id, contest, vote_rank, candidate, precinct) %>%
   spread(key = vote_rank, value = candidate)
 
-# FinalImage is the nice file. We can just run means on it to calculate results.
+# FinalImage is the nice table to look at
 
 UsefulImage <- BallotImage %>%
   select(pref_voter_id, contest, vote_rank, candidate, precinct)
 
 for (j in unique(BallotImage$contest)) {
+  
+# Filter for only the current election
   assign(paste0("Election: ", j),
          UsefulImage %>%
            filter(contest == j))
-  
+
+# Create the base dataframes to attach results to round by round   
   assign("round0", data.frame(unique(get(paste0("Election: ", j))[ ,4])))
   assign(colnames(round0), c("candidate"))
   assign("loser0", data.frame(candidate = character()))
@@ -83,18 +95,27 @@ for (j in unique(BallotImage$contest)) {
     assign(
       paste0("round", i+1),
       get(paste0("Election: ", j)) %>%
+        
+# Remove all previous losers
         filter(!(candidate %in% get(paste0("loser", i))[['candidate']])) %>%
         group_by(pref_voter_id) %>%
+  
+# Pull the lowest vote_rank for each voter, their highest remaining choice
         filter(vote_rank == min(vote_rank)) %>%
         ungroup() %>%
         group_by(candidate) %>%
+  
+# Count vote totals
         summarise(total = n()) %>%
         arrange(desc(total)))
+    
     b <- get(paste0("round", i+1)) %>% filter(!is.na(candidate))
     assign(paste0("round", i+1),
            mutate(
              get(paste0("round", i+1)),
              prop = total/sum(b$total)))
+# Attach vote proportion in addition to total
+    
     assign(
       paste0("loser", i+1),
       get(paste0("round", i+1)) %>%
@@ -103,20 +124,26 @@ for (j in unique(BallotImage$contest)) {
         head(n = 1) %>%
         select(candidate) %>%
         rbind(get(paste0("loser", i))))
+# Determine the loser of that round
+    
     assign(
       paste0("round", i+1),
       left_join(get(paste0("round", i)),
                 get(paste0("round", i+1)),
                 by = "candidate"))
+# Attach the current round to the table of previous rounds
+    
   }
   assign(paste0("Election: ", j, " (Results)"), get(paste0("round", a+1)))
   assign(paste0("Election: ", j, " (Results)"), get(paste0("Election: ", j, " (Results)"))[order(rowSums(is.na(get(paste0("Election: ", j, " (Results)"))))), ])
   assign(paste0("Election: ", j, " (Results)"), get(paste0("Election: ", j, " (Results)")) %>%
            arrange(is.na(candidate)))
+# Arrange the table in order of elimination, with exhausted/invalid votes (NA) at the bottom
+  
 }
 
+# Clear all the extra items out of the environment
 rm(list = ls(pattern = "^loser"))
 rm(list = ls(pattern = "^round"))
 
 # Results are saved, and able to view
-# need to fix colnames and remove NA proportions, as well as mutate to get transfer numbers
